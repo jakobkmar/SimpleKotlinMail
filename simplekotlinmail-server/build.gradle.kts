7dfe03e1728c2d64e035c424b9fa0b821836fef1
bluefireoly <bluefireolymp@gmail.com>
2021-01-08 18:23:18 +0100

    Update build.gradle.kts

--- simplekotlinmail-server/build.gradle.kts
+++ simplekotlinmail-server/build.gradle.kts
@@ -1,3 +1,27 @@
+@file:Suppress("UNUSED_VARIABLE")
+
+/**
+ * PLUGINS
+ */
+
 plugins {
     `common-build-script`
+}
+
+/**
+ * DEPENDENCY MANAGEMENT
+ */
+
+kotlin {
+    sourceSets {
+        val jvmMain by getting {
+
+            dependencies {
+
+
+
+            }
+
+        }
+    }
 }
