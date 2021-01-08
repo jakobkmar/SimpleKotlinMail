2b5ace17754cc3780ddff5b355bd8989a16f7e94
bluefireoly <bluefireolymp@gmail.com>
2021-01-08 19:18:44 +0100

    Add copy methods

--- simplekotlinmail-client/src/jvmMain/kotlin/net/axay/simplekotlinmail/email/EmailBuilder.kt
+++ simplekotlinmail-client/src/jvmMain/kotlin/net/axay/simplekotlinmail/email/EmailBuilder.kt
@@ -2,6 +2,7 @@ package net.axay.simplekotlinmail.email
 
 import org.simplejavamail.api.email.Email
 import org.simplejavamail.api.email.EmailPopulatingBuilder
+import org.simplejavamail.converter.EmailConverter
 import org.simplejavamail.email.EmailBuilder
 
 /**
@@ -11,3 +12,16 @@ import org.simplejavamail.email.EmailBuilder
  */
 inline fun emailBuilder(builder: EmailPopulatingBuilder.() -> Unit): Email =
     EmailBuilder.startingBlank().apply(builder).buildEmail()
+
+/**
+ * Copy this email and open a new email builder.
+ * This function automatically builds the new email and
+ * returns it.
+ */
+inline fun Email.copy(builder: EmailPopulatingBuilder.() -> Unit): Email =
+    EmailBuilder.copying(this).apply(builder).buildEmail()
+
+/**
+ * Copy this email.
+ */
+fun Email.copy(): Email = EmailBuilder.copying(this).buildEmail()
