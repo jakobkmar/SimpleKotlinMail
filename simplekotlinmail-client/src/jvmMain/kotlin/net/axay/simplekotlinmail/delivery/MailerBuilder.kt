c094d87c1831feb7ccfa1c76ea0d21e9e951e40c
bluefireoly <bluefireolymp@gmail.com>
2021-01-09 03:01:27 +0100

    Create MailerBuilder.kt

---
+++ simplekotlinmail-client/src/jvmMain/kotlin/net/axay/simplekotlinmail/delivery/MailerBuilder.kt
@@ -0,0 +1,33 @@
+package net.axay.simplekotlinmail.delivery
+
+import net.axay.simplekotlinmail.data.SMTPLoginInfo
+import org.simplejavamail.api.mailer.Mailer
+import org.simplejavamail.mailer.MailerBuilder
+import org.simplejavamail.mailer.internal.MailerRegularBuilderImpl
+
+/**
+ * Open a mailer builder.
+ * This function automatically builds the net.axay.simplekotlinmail.email and
+ * returns it.
+ */
+inline fun mailerBuilder(smtpLoginInfo: SMTPLoginInfo, builder: MailerRegularBuilderImpl.() -> Unit): Mailer =
+    MailerBuilder
+        .withSMTPServer(smtpLoginInfo.host, smtpLoginInfo.port, smtpLoginInfo.username, smtpLoginInfo.password)
+        .apply(builder)
+        .buildMailer()
+
+/**
+ * Open a mailer builder.
+ * This function automatically builds the net.axay.simplekotlinmail.email and
+ * returns it.
+ */
+inline fun mailerBuilder(
+    host: String?,
+    port: Int?,
+    username: String?,
+    password: String?,
+    builder: MailerRegularBuilderImpl.() -> Unit
+): Mailer = MailerBuilder
+    .withSMTPServer(host, port, username, password)
+    .apply(builder)
+    .buildMailer()
