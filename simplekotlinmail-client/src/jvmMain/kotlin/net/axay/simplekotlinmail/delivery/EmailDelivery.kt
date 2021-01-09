16f4a57d598ed7955882c05f8c4f9aa9e15eebe1
bluefireoly <bluefireolymp@gmail.com>
2021-01-09 02:07:38 +0100

    Create EmailDelivery.kt

---
+++ simplekotlinmail-client/src/jvmMain/kotlin/net/axay/simplekotlinmail/delivery/EmailDelivery.kt
@@ -0,0 +1,33 @@
+package net.axay.simplekotlinmail.delivery
+
+import kotlinx.coroutines.*
+import org.simplejavamail.api.email.Email
+import org.simplejavamail.api.mailer.Mailer
+import org.simplejavamail.mailer.MailerBuilder
+
+/**
+ * The default mailer instance, that is used if
+ * the no other instance is provided.
+ */
+val DEFAULT_MAILER: Mailer = MailerBuilder
+    .withSMTPServer("localhost", 25)
+    .buildMailer()
+
+/**
+ * Send this email.
+ * @param mailer the mailer which should be used to deliver the message
+ * @param onException optional callback which should be executed if an error occurs
+ * @param onSuccess optional callback which should be executed if the message was delivered successfully
+ */
+suspend fun Email.send(
+    mailer: Mailer = DEFAULT_MAILER,
+    onException: suspend (Exception) -> Unit = {},
+    onSuccess: suspend () -> Unit = {}
+) = withContext(Dispatchers.IO) {
+    try {
+        mailer.sendMail(this@send)
+        onSuccess()
+    } catch (exc: Exception) {
+        onException(exc)
+    }
+}
