54b4a0d7d20e59a10fdb224d6b524122fc64b7f1
bluefireoly <bluefireolymp@gmail.com>
2021-01-09 01:04:11 +0100

    Create SMTPLoginInfo.kt

---
+++ simplekotlinmail-core/src/jvmMain/kotlin/net/axay/simplekotlinmail/data/SMTPLoginInfo.kt
@@ -0,0 +1,11 @@
+package net.axay.simplekotlinmail.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SMTPLoginInfo(
+    val host: String?,
+    val port: Int?,
+    val username: String?,
+    val password: String?
+)
