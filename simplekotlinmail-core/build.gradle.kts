be556a107c9a66746c506db2fb7aa44a78cb8043
bluefireoly <bluefireolymp@gmail.com>
2021-01-09 17:19:54 +0100

    Update build.gradle.kts

--- simplekotlinmail-core/build.gradle.kts
+++ simplekotlinmail-core/build.gradle.kts
@@ -37,7 +37,7 @@ kotlin {
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 
                 // SERIALIZATION
-                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
 
             }
 
