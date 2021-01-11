87d3c015c21082f2503e7bf500c40ef3400bab3b
bluefireoly <bluefireolymp@gmail.com>
2021-01-11 01:59:38 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ val projectName = "simplekotlinmail"
 
 rootProject.name = projectName
 
+include("$projectName-core")
+
 include("$projectName-server")
 include("$projectName-client")
-include("$projectName-core")
+include("$projectName-html")
