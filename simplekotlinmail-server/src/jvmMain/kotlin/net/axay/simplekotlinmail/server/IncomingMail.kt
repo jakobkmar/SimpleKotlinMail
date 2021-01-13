5c6ccb5d8b90cd1efe02792ee6b216dd5b00d116
bluefireoly <bluefireolymp@gmail.com>
2021-01-13 20:44:19 +0100

    Update IncomingMail.kt

--- simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/IncomingMail.kt
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/IncomingMail.kt
@@ -3,6 +3,9 @@ package net.axay.simplekotlinmail.server
 import org.simplejavamail.api.email.Email
 import org.subethamail.smtp.MessageContext
 
+/**
+ * This class represents an incoming mail, received by a SMTP server.
+ */
 data class IncomingMail(
     val envelopeFrom: String,
     val recipients: List<String>,
