0238eb91a7482619f5e5e1dbeaf66298effc6a45
bluefireoly <bluefireolymp@gmail.com>
2021-01-13 19:42:05 +0100

    Add protocol versions to setupTLS function

--- simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/tls/SMTPServerTLS.kt
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/tls/SMTPServerTLS.kt
@@ -9,30 +9,24 @@ import javax.net.ssl.SSLSocket
  * Setup TLS for this smtp server.
  *
  * @param tlsContext The SSLContext for creating a SSLSocket. Use [TLSContext] to easily create a new context
+ * @param protocolVersions The TLS protocol versions you wish to enable.
  * @param requireClientAuth [SSLSocket.setNeedClientAuth]
  * @param builder (optional) Use this builder to configure the SSLSocket
  */
 fun SMTPServer.Builder.setupTLS(
     tlsContext: SSLContext,
+    protocolVersions: List<TLSVersions> = listOf(TLSVersions.TLS_1_3, TLSVersions.TLS_1_2),
     requireClientAuth: Boolean = true,
     builder: SSLSocket.() -> Unit = {}
 ): SMTPServer.Builder =
-    startTlsSocketFactory {
+    startTlsSocketFactory { socketIn ->
         (tlsContext.socketFactory.createSocket(
-            it, (it.remoteSocketAddress as InetSocketAddress).hostString, it.port, true
+            socketIn, (socketIn.remoteSocketAddress as InetSocketAddress).hostString, socketIn.port, true
         ) as SSLSocket).apply {
             useClientMode = false
 
-            enabledProtocols = arrayOf(
-                "TLSv1.3",
-                "TLSv1.2"
-            )
-            enabledCipherSuites = arrayOf(
-                "TLS_AES_128_GCM_SHA256",
-                "TLS_AES_256_GCM_SHA384",
-                "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
-            )
+            enabledProtocols = protocolVersions.mapTo(LinkedHashSet()) { it.protocolVersion }.toTypedArray()
+            enabledCipherSuites = protocolVersions.flatMapTo(LinkedHashSet()) { it.cipherSuites }.toTypedArray()
 
             needClientAuth = requireClientAuth
 
