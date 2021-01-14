080f33c82540104a861b19ba46ee591c4f0d9707
bluefireoly <bluefireolymp@gmail.com>
2021-01-14 16:40:44 +0100

    Update TLSVersions.kt

--- simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/tls/TLSVersions.kt
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/tls/TLSVersions.kt
@@ -19,7 +19,8 @@ enum class TLSVersions(
         "TLSv1.3",
         listOf(
             "TLS_AES_256_GCM_SHA384",
-            "TLS_AES_128_GCM_SHA256"
+            "TLS_AES_128_GCM_SHA256",
+            "TLS_CHACHA20_POLY1305_SHA256"
         )
     ),
 
@@ -27,7 +28,8 @@ enum class TLSVersions(
         "TLSv1.2",
         listOf(
             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
         )
     ),
 
