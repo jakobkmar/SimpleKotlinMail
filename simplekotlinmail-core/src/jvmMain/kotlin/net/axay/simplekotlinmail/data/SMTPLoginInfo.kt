da25e81c0bc3ffb14b57e0bebecd6bf1570247b0
bluefireoly <bluefireolymp@gmail.com>
2021-01-20 17:53:51 +0100

    Update SMTPLoginInfo.kt

--- simplekotlinmail-core/src/jvmMain/kotlin/net/axay/simplekotlinmail/data/SMTPLoginInfo.kt
+++ simplekotlinmail-core/src/jvmMain/kotlin/net/axay/simplekotlinmail/data/SMTPLoginInfo.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SMTPLoginInfo(
-    val host: String?,
-    val port: Int?,
+    val host: String,
+    val port: Int,
     val username: String?,
     val password: String?
 )
