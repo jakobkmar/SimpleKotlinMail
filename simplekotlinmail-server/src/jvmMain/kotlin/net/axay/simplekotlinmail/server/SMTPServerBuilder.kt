ef93e3c1a0ec4b8a1c69b367f37f2e852c11686e
bluefireoly <bluefireolymp@gmail.com>
2021-01-22 20:04:32 +0100

    Update SMTPServerBuilder.kt

--- simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/SMTPServerBuilder.kt
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/SMTPServerBuilder.kt
@@ -2,14 +2,38 @@ package net.axay.simplekotlinmail.server
 
 import org.subethamail.smtp.server.SMTPServer
 
-/**
- * Set the MailListener for this SMTPServer.
- */
-fun SMTPServer.Builder.mailListener(listener: MailListener): SMTPServer.Builder =
-    messageHandlerFactory(MailHandlerFactory(listener))
-
 class SMTPServerBuilder internal constructor(port: Int) {
+    private var fromListener: FromListener? = null
+    private var recipientListener: RecipientListener? = null
+    private var mailListener: MailListener? = null
 
     internal val builder: SMTPServer.Builder = SMTPServer.port(port)
 
+    /**
+     * Set the FromListener for this SMTPServer.
+     */
+    fun fromListener(listener: FromListener) {
+        fromListener = listener
+    }
+
+    /**
+     * Set the RecipientListener for this SMTPServer.
+     */
+    fun recipientListener(listener: RecipientListener) {
+        recipientListener = listener
+    }
+
+    /**
+     * Set the MailListener for this SMTPServer.
+     */
+    fun mailListener(listener: MailListener) {
+        mailListener = listener
+    }
+
+    internal fun build(): SMTPServer {
+        if (fromListener != null || recipientListener != null || mailListener != null)
+            builder.messageHandlerFactory(MailHandlerFactory(fromListener, recipientListener, mailListener))
+
+        return builder.build()
+    }
 }
