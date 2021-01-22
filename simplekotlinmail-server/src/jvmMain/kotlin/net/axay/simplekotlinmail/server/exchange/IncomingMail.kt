b861ccdda8e9140067a39340014408e586c6eae3
bluefireoly <bluefireolymp@gmail.com>
2021-01-22 19:45:13 +0100

    Update IncomingMail.kt

--- simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/exchange/IncomingMail.kt
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/exchange/IncomingMail.kt
@@ -2,13 +2,37 @@ package net.axay.simplekotlinmail.server.exchange
 
 import org.simplejavamail.api.email.Email
 import org.subethamail.smtp.MessageContext
+import org.subethamail.smtp.TooMuchDataException
 
 /**
  * This class represents an incoming mail, received by a SMTP server.
  */
-data class IncomingMail(
+class IncomingMail internal constructor(
     val envelopeFrom: String,
     val recipients: List<String>,
     val email: Email,
-    val context: MessageContext
-)
+    context: MessageContext
+) : IncomingMailExchange(context) {
+
+    internal var response: String? = null
+
+    private var ifTooMuchData = false
+    private var tooMuchDataMessage: String? = null
+
+    fun respondText(message: String) {
+        response = message
+    }
+
+    fun tooMuchData(message: String? = null) {
+        ifTooMuchData = true
+        tooMuchDataMessage = message
+    }
+
+    override fun throwSpecificExceptions() {
+        if (ifTooMuchData)
+            if (tooMuchDataMessage != null)
+                throw TooMuchDataException(tooMuchDataMessage)
+            else throw TooMuchDataException()
+    }
+
+}
