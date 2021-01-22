e3ca166a81f8cb65d07d3136266e4905d8356e48
bluefireoly <bluefireolymp@gmail.com>
2021-01-22 19:44:13 +0100

    Create IncomingRecipient.kt

---
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/exchange/IncomingRecipient.kt
@@ -0,0 +1,9 @@
+package net.axay.simplekotlinmail.server.exchange
+
+import org.subethamail.smtp.MessageContext
+
+class IncomingRecipient internal constructor(
+    val recipient: String,
+    val currentRecipients: List<String>,
+    context: MessageContext
+) : IncomingMailExchange(context)
