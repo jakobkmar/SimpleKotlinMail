c3d13143fff36e884250b81f91db68d49d6debdf
bluefireoly <bluefireolymp@gmail.com>
2021-01-22 19:42:24 +0100

    Create IncomingMailExchange.kt

---
+++ simplekotlinmail-server/src/jvmMain/kotlin/net/axay/simplekotlinmail/server/exchange/IncomingMailExchange.kt
@@ -0,0 +1,43 @@
+package net.axay.simplekotlinmail.server.exchange
+
+import org.subethamail.smtp.DropConnectionException
+import org.subethamail.smtp.MessageContext
+import org.subethamail.smtp.RejectException
+
+abstract class IncomingMailExchange(val context: MessageContext) {
+
+    private var ifReject = false
+    private var rejectMessage: String? = null
+    private var rejectCode: Int? = null
+
+    private var ifDropConnection = false
+    private var dropConnectionMessage: String? = null
+    private var dropConnectionCode: Int? = null
+
+    fun reject(message: String? = null, statusCode: Int? = null) {
+        ifReject = true
+        rejectMessage = message
+        rejectCode = statusCode
+    }
+
+    fun dropConnection(message: String? = null, statusCode: Int? = null) {
+        ifDropConnection = true
+    }
+
+    internal fun throwExceptions() {
+        if (ifReject)
+            throw RejectException(
+                rejectCode ?: RejectException.DEFAULT_CODE,
+                rejectMessage ?: RejectException.DEFAULT_MESSAGE
+            )
+        if (ifDropConnection)
+            throw DropConnectionException(
+                dropConnectionCode ?: DropConnectionException.DEFAULT_CODE,
+                dropConnectionMessage ?: DropConnectionException.DEFAULT_MESSAGE
+            )
+        throwSpecificExceptions()
+    }
+
+    protected open fun throwSpecificExceptions() {}
+
+}
