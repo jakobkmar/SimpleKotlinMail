c1f9678967a596f098927166c1a8dd8f3fe37321
bluefireoly <bluefireolymp@gmail.com>
2021-01-24 18:45:21 +0100

    Update ServerClientTest.kt

--- simplekotlinmail-test/src/jvmTest/kotlin/net/axay/simplekotlinmail/test/ServerClientTest.kt
+++ simplekotlinmail-test/src/jvmTest/kotlin/net/axay/simplekotlinmail/test/ServerClientTest.kt
@@ -5,43 +5,74 @@ import net.axay.simplekotlinmail.delivery.sendSync
 import net.axay.simplekotlinmail.email.emailBuilder
 import net.axay.simplekotlinmail.server.smtpServer
 import net.axay.simplekotlinmail.server.start
-import org.junit.jupiter.api.Assertions
+import net.axay.simplekotlinmail.server.tls.TLSContext
+import net.axay.simplekotlinmail.server.tls.setupTLS
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
+import java.io.File
 
 class ServerClientTest {
+
+    private val plainText = "Hey, how are you?"
+
+    private val email = emailBuilder {
+        from("foo@bar.com")
+        to("info@example.org")
+
+        withSubject("This is an important message!")
+        withPlainText(plainText)
+    }
 
     @Test
     fun testServer() {
 
-        assertDoesNotThrow {
-
-            val plainText = "Hey, how are you?"
-
-            val smtpServer = smtpServer {
-                mailListener {
-                    println("received email: ${it.email.plainText}")
-                    Assertions.assertEquals(it.email.plainText?.trim(), plainText)
-                }
+        val smtpServer = smtpServer {
+            mailListener {
+                println("received email: ${it.email.plainText}")
+                assertEquals(it.email.plainText?.trim(), plainText)
             }
-
-            smtpServer.start(keepAlive = true)
-
-            val email = emailBuilder {
-                from("foo@bar.com")
-                to("info@example.org")
-
-                withSubject("This is an important message!")
-                withPlainText(plainText)
-            }
-
-            email.sendSync()
-
-            MailerManager.shutdownMailers()
-
-            smtpServer.stop()
-
         }
+
+        smtpServer.start(keepAlive = true)
+
+        email.sendSync()
+
+        MailerManager.shutdownMailers()
+
+        smtpServer.stop()
+
+    }
+
+    @Test
+    fun testServerTLS() {
+
+        println(File("./src/jvmTest/resources/keystore").absoluteFile.absolutePath)
+
+        val plainText = "Hey, how are you?"
+
+        val smtpServer = smtpServer {
+            setupTLS(
+                TLSContext(
+                    File("./src/jvmTest/resources/keystore"), "passphrase",
+                    File("./src/jvmTest/resources/truststore"), "passphrase"
+                ),
+                true,
+                requireClientAuth = false
+            )
+
+            mailListener {
+                println("received email: ${it.email.plainText}")
+                assertEquals(it.email.plainText?.trim(), plainText)
+            }
+        }
+
+        smtpServer.start(keepAlive = true)
+
+        email.sendSync()
+
+        MailerManager.shutdownMailers()
+
+        smtpServer.stop()
 
     }
 
