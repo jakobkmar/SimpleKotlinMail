c29538d6c62aecefab802c4d980a61a071b655af
bluefireoly <bluefireolymp@gmail.com>
2021-01-25 17:20:18 +0100

    Update EmailDelivery.kt

--- simplekotlinmail-client/src/jvmMain/kotlin/net/axay/simplekotlinmail/delivery/EmailDelivery.kt
+++ simplekotlinmail-client/src/jvmMain/kotlin/net/axay/simplekotlinmail/delivery/EmailDelivery.kt
@@ -7,20 +7,26 @@ import org.simplejavamail.api.mailer.Mailer
 /**
  * Send this email.
  * @param mailer the mailer which should be used to deliver the message
+ * @param awaitCompletion if true, the function will suspend until the message was sent
  * @param onException optional callback which should be executed if an error occurs
  * @param onSuccess optional callback which should be executed if the message was delivered successfully
  */
 suspend fun Email.send(
     mailer: Mailer = MailerManager.defaultMailer,
+    awaitCompletion: Boolean = false,
     onException: suspend (Exception) -> Unit = {},
     onSuccess: suspend () -> Unit = {}
-) = withContext(Dispatchers.IO) {
-    try {
-        mailer.sendMail(this@send, false)
-        onSuccess()
-    } catch (exc: Exception) {
-        onException(exc)
+) = coroutineScope {
+    val job = launch(Dispatchers.IO) {
+        try {
+            mailer.sendMail(this@send, false)
+            onSuccess()
+        } catch (exc: Exception) {
+            onException(exc)
+        }
     }
+    if (awaitCompletion)
+        job.join()
 }
 
 /**
