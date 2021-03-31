e6cbf41a577d970283ff4f49583756ba97ed85ca
Jakob K <bluefireolymp@gmail.com>
2021-03-31 15:26:22 +0200

    Update common-build-script.gradle.kts

--- buildSrc/src/main/kotlin/common-build-script.gradle.kts
+++ buildSrc/src/main/kotlin/common-build-script.gradle.kts
@@ -5,7 +5,7 @@ object Versions {
 val repoName = "SimpleKotlinMail"
 
 group = "net.axay"
-version = "1.3.3"
+version = "1.3.2"
 
 description = "A simple, modern and coroutine based Kotlin Email API, supporting both clientside and serverside projects"
 
