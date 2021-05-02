1ee1e1a7a3b9969c52aaa7966591ba2994640be0
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-02 23:50:57 +0200

    Update ServerClientTest.kt

--- simplekotlinmail-test/src/test/kotlin/net/axay/simplekotlinmail/test/ServerClientTest.kt
+++ simplekotlinmail-test/src/test/kotlin/net/axay/simplekotlinmail/test/ServerClientTest.kt
@@ -2,18 +2,21 @@ package net.axay.simplekotlinmail.test
 
 import kotlinx.coroutines.runBlocking
 import net.axay.simplekotlinmail.delivery.MailerManager
-import net.axay.simplekotlinmail.delivery.send
+import net.axay.simplekotlinmail.delivery.mailerBuilder
+import net.axay.simplekotlinmail.delivery.sendSync
 import net.axay.simplekotlinmail.email.emailBuilder
 import net.axay.simplekotlinmail.server.smtpServer
 import net.axay.simplekotlinmail.server.start
 import net.axay.simplekotlinmail.server.tls.TLSContext
 import net.axay.simplekotlinmail.server.tls.setupTLS
+import net.axay.simplekotlinmail.server.utils.session
+import net.axay.simplekotlinmail.server.utils.sslSocket
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.simplejavamail.api.mailer.config.TransportStrategy
 import java.io.File
 
 class ServerClientTest {
-
     private val plainText = "Hey, how are you?"
 
     private val email = emailBuilder {
@@ -26,8 +29,7 @@ class ServerClientTest {
 
     @Test
     fun testServer() = runBlocking {
-
-        val smtpServer = smtpServer {
+        val smtpServer = smtpServer(2500) {
             mailListener {
                 println("received email: ${it.email.plainText}")
                 assertEquals(it.email.plainText?.trim(), plainText)
@@ -36,43 +38,43 @@ class ServerClientTest {
 
         smtpServer.start(keepAlive = true)
 
-        email.send(awaitCompletion = true)
+        email.sendSync(mailerBuilder(port = 2500))
 
         MailerManager.shutdownMailers()
 
         smtpServer.stop()
-
     }
 
     @Test
     fun testServerTLS() = runBlocking {
-
-        val plainText = "Hey, how are you?"
-
-        val smtpServer = smtpServer {
+        val smtpServer = smtpServer(2500) {
             setupTLS(
                 TLSContext(
                     File("./src/test/resources/keystore"), "passphrase",
                     File("./src/test/resources/truststore"), "passphrase"
                 ),
                 true,
+                starttlsOnly = false,
                 requireClientAuth = false
             )
 
             mailListener {
                 println("received email: ${it.email.plainText}")
                 assertEquals(it.email.plainText?.trim(), plainText)
+                assert(it.context.session?.sslSocket != null)
             }
         }
 
         smtpServer.start(keepAlive = true)
 
-        email.send(awaitCompletion = true)
+        email.sendSync(mailerBuilder(port = 2500) {
+            verifyingServerIdentity(false)
+            trustingAllHosts(true)
+            withTransportStrategy(TransportStrategy.SMTPS)
+        })
 
         MailerManager.shutdownMailers()
 
         smtpServer.stop()
-
     }
-
 }
