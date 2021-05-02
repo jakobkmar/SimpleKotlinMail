7d9519d9d87527678255ec3b5b63627cd5ad8aca
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-02 18:07:17 +0200

    Add support for full TLS sockets

--- simplekotlinmail-server/src/main/kotlin/net/axay/simplekotlinmail/server/tls/SMTPServerTLS.kt
+++ simplekotlinmail-server/src/main/kotlin/net/axay/simplekotlinmail/server/tls/SMTPServerTLS.kt
@@ -3,6 +3,7 @@ package net.axay.simplekotlinmail.server.tls
 import net.axay.simplekotlinmail.server.SMTPServerBuilder
 import java.net.InetSocketAddress
 import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLServerSocket
 import javax.net.ssl.SSLSocket
 
 /**
@@ -14,17 +15,44 @@ import javax.net.ssl.SSLSocket
  * @param overrideCipherSuites if true, the default enabled cipher suites will be replaced with
  * the cipher suites chosen by SimpleKotlinMail - you can see them here: [TLSVersions]
  * @param requireClientAuth [SSLSocket.setNeedClientAuth]
- * @param socketBuilder (optional) use this builder to configure the SSLSocket
+ * @param starttlsOnly enable this, if you wish to only use the sometimes deprecated STARTTLS
+ * variant of TLS
+ * @param tlsSocketBuilder use this builder to configure the TLS socket
+ * @param starttlsSocketBuilder use this builder to configure the STARTTLS socket
  */
 fun SMTPServerBuilder.setupTLS(
     tlsContext: SSLContext,
     requireTLS: Boolean = false,
-    protocolVersions: List<TLSVersions> = listOf(TLSVersions.TLS_1_3, TLSVersions.TLS_1_2),
+    protocolVersions: Array<TLSVersions> = arrayOf(TLSVersions.TLS_1_3, TLSVersions.TLS_1_2),
     overrideCipherSuites: Boolean = false,
     requireClientAuth: Boolean = true,
-    socketBuilder: (SSLSocket.() -> Unit)? = null
+    starttlsOnly: Boolean = false,
+    tlsSocketBuilder: (SSLServerSocket.() -> Unit)? = null,
+    starttlsSocketBuilder: (SSLSocket.() -> Unit)? = null,
 ) {
     if (requireTLS) builder.requireTLS() else builder.enableTLS()
+
+    if (!starttlsOnly) builder.serverSocketFactory {
+        (tlsContext.serverSocketFactory.createServerSocket() as SSLServerSocket).apply {
+            useClientMode = false
+
+            enabledProtocols = (
+                    protocolVersions
+                        .mapTo(LinkedHashSet()) { it.protocolVersion } intersect supportedProtocols.toList()
+                    ).toTypedArray()
+
+            if (overrideCipherSuites) {
+                enabledCipherSuites = (
+                        protocolVersions
+                            .flatMapTo(LinkedHashSet()) { it.cipherSuites } intersect supportedCipherSuites.toList()
+                        ).toTypedArray()
+            }
+
+            needClientAuth = requireClientAuth
+
+            tlsSocketBuilder?.invoke(this)
+        }
+    }
 
     builder.startTlsSocketFactory { socketIn ->
         (tlsContext.socketFactory.createSocket(
@@ -46,7 +74,7 @@ fun SMTPServerBuilder.setupTLS(
 
             needClientAuth = requireClientAuth
 
-            socketBuilder?.invoke(this)
+            starttlsSocketBuilder?.invoke(this)
         }
     }
 }
